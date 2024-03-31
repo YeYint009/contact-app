f8a82f8ca3fcb39ef833a7bbe24b9a2b9e5a4fa5
Ye Yint <blackskym96@gmail.com>
2024-04-01 03:18:12 +0900

    yarn.lock updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from "react-dom/client";
+import ReactDom from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
+ReactDom.createRoot(document.querySelector("#root")).render(<App />);
